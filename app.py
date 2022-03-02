7c1159ec2552d0edafe784ada6b4830a05ee7261
Aneesh Pavan Prodduturu <43541169+aneeshpavan@users.noreply.github.com>
2022-03-02 17:59:52 +0530

    Test(1)

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, render_template, url_for, request
+import qrcode
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+@app.route('/result',methods=['POST', 'GET'])
+def result():
+        qwerty = request.form.get("link")
+        asdf = qrcode.make(qwerty)  
+        print(asdf)
+        asdf.save("static/qr.jpg")
+        return render_template('index.html',asdf = asdf)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
